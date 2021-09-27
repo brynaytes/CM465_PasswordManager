88b25f7c976fe27a8e192cde508b227674335e38
brynaytes <45157398+brynaytes@users.noreply.github.com>
2021-09-27 15:54:27 -0500

    Creating team directory

---
+++ capstone - team/Main/Capstone/src/module-info.java
@@ -0,0 +1,8 @@
+module Capstone {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires javafx.base;
+	
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
+}
