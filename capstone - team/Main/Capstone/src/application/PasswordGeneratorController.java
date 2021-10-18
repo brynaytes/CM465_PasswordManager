93ba380ec452cf30ca4814775d5c168d543b58bb
Jessie Chung <91212975+JessieC465@users.noreply.github.com>
2021-10-17 22:03:01 -0500

    Update PasswordGeneratorController.java
    -removed lowercase checkbox
    -added word checkbox and boolean value (wd)
    -password length choicebox now goes up to 64
    -password length choicebox now defaults to 8

--- capstone - team/Main/Capstone/src/application/PasswordGeneratorController.java
+++ capstone - team/Main/Capstone/src/application/PasswordGeneratorController.java
@@ -31,22 +31,29 @@ public class PasswordGeneratorController {
 	@FXML
 	private CheckBox numbers_chkbx;
 	@FXML
-	private CheckBox lowercase_chkbx;
-	@FXML
 	private CheckBox uppercase_chkbx;
+	@FXML
+	private CheckBox word_chkbx;
 	
 	public void initialize()
 	{
 		passwordLength_cb.getItems().clear();
 		passwordLength_cb.getItems().addAll(
-				4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+				4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
+				31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,
+				56,57,58,59,60,61,62,63,64);
+		passwordLength_cb.setValue(8);
 	}
 	
 	public void onClick_btn_GeneratePassword(ActionEvent event)
 	{
 		try {
-			int passlength = passwordLength_cb.getValue() - 1;
-			String password = PasswordLogic.makePassword(passlength);
+			int passlength = passwordLength_cb.getValue();
+			boolean uc = uppercase_chkbx.isSelected();
+			boolean sy = symbols_chkbx.isSelected();
+			boolean nb = numbers_chkbx.isSelected();
+			boolean wd = word_chkbx.isSelected();
+			String password = PasswordLogic.makePassword(passlength, uc, sy, nb, wd);
 			password_txt.setText(password);
 		}
 		catch (Exception e) {
