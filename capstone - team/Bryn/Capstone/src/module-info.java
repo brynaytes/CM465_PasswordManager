d409b0fcb28fe8f89a0669003d3fe171ac879c36
brynaytes <45157398+brynaytes@users.noreply.github.com>
2021-10-18 21:33:04 -0500

    removing for space reasons

--- capstone - team/Bryn/Capstone/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Capstone {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml, javafx.base;
-}
