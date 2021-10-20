e5ccd9dfd99ccd4a7a53c64fa270141af174aa5f
Jessie Chung <91212975+JessieC465@users.noreply.github.com>
2021-10-19 21:38:52 -0500

    Update PasswordLogic.java
    -changed logic to include lowercase letter exclusion
    -added alert dialog for when the user excludes every character type and dictionary word option
    -added method that reads from a file with every English word and inserts a random one in the beginning of the generated password

--- capstone - team/Main/Capstone/src/application/PasswordLogic.java
+++ capstone - team/Main/Capstone/src/application/PasswordLogic.java
@@ -1,15 +1,21 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Random;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class PasswordLogic {
 	
-	public static String makePassword(int length, boolean uc, boolean sy, boolean nb, boolean wd)
+	public static String makePassword(int length, boolean uc, boolean lc, boolean sy, boolean nb, boolean wd)
 	{
 		String password = "";
 
-		for(int i = 0; i < length/4; i++) {
-			
-			String randomLowercase =  randomCharacter("abcdefghijklmnopqrstuvwxyz");
-			password = insertAtRandom(password, randomLowercase);
+		while(password.length() < length) {
 
 			if(nb == false) {
 				String randomDigit = randomCharacter("0123456789");
@@ -17,7 +23,7 @@ public class PasswordLogic {
 			}
 
 			if(sy == false) {
-				String randomSymbol = randomCharacter("+-*/?!@#$%&");
+				String randomSymbol = randomCharacter("`~!@#$%^&*()-_=+|}{][;:',<.>/?");
 				password = insertAtRandom(password, randomSymbol);
 			}
 
@@ -25,20 +31,25 @@ public class PasswordLogic {
 				String randomUppercase = randomCharacter("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 				password = insertAtRandom(password, randomUppercase);
 			}
+			
+			if(lc == false) {
+				String randomLowercase =  randomCharacter("abcdefghijklmnopqrstuvwxyz");
+				password = insertAtRandom(password, randomLowercase);
+			}
+			
+			if(nb == true && sy == true && uc == true && lc == true && wd == false) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setHeaderText("No Items Selected");
+				alert.setContentText("Please include at least one character type or word.");
+				alert.show();
+				break;
+			}
+			
+			if(nb == true && sy == true && uc == true && lc == true && wd == true) {
+				break;
+			}
+			
 		}
-		
-		int passLength1 = password.length();
-	    int rest = length - passLength1;
-	    for (int i = 0; i < rest; i++)	{
-	        String randomLowercase =  randomCharacter("abcdefghijklmnopqrstuvwxyz");
-			password = insertAtRandom(password, randomLowercase);
-	    }
-	    
-	    if (wd == true) {
-	    	//will figure out how to get random dictionary words later
-        	password = "test" + password;
-        }
-	    
 		return password;
 	}
 	
@@ -53,4 +64,20 @@ public class PasswordLogic {
 		int r = (int)((n + 1) * Math.random());
 		return str.substring(0, r) + toInsert + str.substring(r);
 	}	
+	
+	public static String dictionaryWord(String password) {
+
+		List<String> lines = null;
+		try {
+			lines = Files.readAllLines(new File("words.txt").toPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		final Random rand = new Random();
+		String word = lines.get(rand.nextInt(lines.size()));
+        password = word + password;
+
+		return password;
+	}
 }
