b1628b2b0d5148747a231d215c69ef3aa6a9a1b2
Msmith9375790 <38225022+Msmith9375790@users.noreply.github.com>
2021-10-25 10:20:46 -0500

    Update module-info.java
    Adding "requires mysql.connector.java;" and "requires java.sql;"

--- capstone - team/Main/Capstone/src/module-info.java
+++ capstone - team/Main/Capstone/src/module-info.java
@@ -3,6 +3,8 @@ module Capstone {
 	requires javafx.graphics;
 	requires javafx.fxml;
 	requires javafx.base;
+	requires mysql.connector.java;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml, javafx.base;
 }
