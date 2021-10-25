43fbd89f2c913d30352a01dbc9a5253e4ecd7f63
Msmith9375790 <38225022+Msmith9375790@users.noreply.github.com>
2021-10-25 08:53:57 -0500

    Update CredentialsTableViewController.java
    The TableView now reads and populates from the Amazon hosted database.

--- capstone - team/Main/Capstone/src/application/CredentialsTableViewController.java
+++ capstone - team/Main/Capstone/src/application/CredentialsTableViewController.java
@@ -2,6 +2,9 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -47,8 +50,21 @@ public class CredentialsTableViewController implements Initializable {
 	
 	public ObservableList<Credential> getCredentials()
 	{
+		ArrayList<Credential> arr = new ArrayList<Credential>();
 		try {
-			return (FXCollections.observableList(ReadingText.reader()));
+			//Connection to the Database
+			Connection c = DBUtil.getDataSource().getConnection();
+			//SQL for selecting the columns we want from the credential table
+			String SQL = "SELECT url, username, password FROM credential";
+			//ResultSet
+			ResultSet rs = c.createStatement().executeQuery(SQL);
+			
+			while(rs.next()) {
+				Credential cred = new Credential(rs.getString(1),rs.getString(2),rs.getString(3));
+				arr.add(cred);
+			}
+			
+			return (FXCollections.observableList(arr));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
